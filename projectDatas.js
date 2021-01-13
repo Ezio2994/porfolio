00f94a83f955d5a526089a25d6d53b001d4a21db
Ezio Intravaia <ezio.intravaia@hotmail.com>
2021-01-13 17:04:34 +0000

    improved js

---
+++ projectDatas.js
@@ -0,0 +1,87 @@
+const projects = [
+    {
+        name: "Happy",
+        urlImg: "./img/happy-app.jpg",
+        description: `A Mental Health App monitoring happiness levels over time, built for a client over 
+        4 weeks in a team of 12. <br> My collaboration on the project consisted on building some components and 
+        working features among them; writing code to get and store datas.`,
+        tags: ["React", "Firebase", "Sass"],
+        live: "https://happy-app-dbb3b.web.app/",
+        gitHub: "",
+    },
+    {
+        name: "Initial Fantasy",
+        urlImg: "./img/rpgGame.jpg",
+        description: `I built this game having in mind the old fashioned turn based rpg games therefore I called it "Initial Fantasy" inspired from one of the greatest fantasy media franchise ever, Final Fantasy. <br> The game is fully built in vanilla JavaScript with a functional programming approach and styled in Sass.`,
+        tags: ["HTML", "Sass", "JavaScript"],
+        live: "http://initialfantasy.intravaiaezio.co.uk/",
+        gitHub: "https://github.com/Ezio2994/js-game",
+    },
+    {
+        name: "The Sicilian Shop",
+        urlImg: "./img/The-sicilian-shop.jpeg",
+        description: `I've built this app to showcase my skills on react, The app is usable either if
+        signing in with google or as a user guest, used API's to
+        get user IP addres to keep record of user guest cart. The database is built on Firebase and
+        it keeps track
+        on the quantity of the products in stock, whenever the product it's out of stock user cannot
+        buy it
+        anymore avoiding to go understock.`,
+        tags: ["React", "Sass", "Firebase"],
+        live: "https://thesicilianshop.intravaiaezio.co.uk/",
+        gitHub: "https://github.com/Ezio2994/react-shop",
+    },
+    {
+        name: "The VideoGames Api",
+        urlImg: "./img/gaming_BG.jpg",
+        description: `With my foundamental on node.js I built my first api and posted on heroku so that 
+        is accessable for everybody but the datas can only be modify by me or whoever I give the private-key
+        access. <br> I've also build a react app to instruct users on how to use this api end-points and 
+        showcase the obtanable data.`,
+        tags: ["React", 'node.js', "Firebase", "Sass"],
+        live: "https://the-videogame-api.intravaiaezio.co.uk/",
+        gitHub: "https://github.com/Ezio2994/the-videogames-api-react",
+    },
+    {
+        name: "Music Quiz",
+        urlImg: "./img/music-quiz.jpg",
+        description: `I have created this interactive music quiz as I love music and have always liked
+        to be challenged in my music knowledges. <br> The game is both single and multiplayer, so you can
+        challenge your friends. <br> The game is fully built in vanilla JavaScript with a functional
+        programming approach and styled in Sass.`,
+        tags: ["HTML", "Sass", "JavaScript"],
+        live: "http://the-music-quiz.intravaiaezio.co.uk/",
+        gitHub: "https://github.com/Ezio2994/music-quiz",
+    },
+    {
+        name: "The Hangman Game",
+        urlImg: "./img/hangman.jpg",
+        description: `I've build this timeless game as I have always enjoyed playing it with
+        friends,hope you will have as much fun playing it as I had building it! <br> Game built in 
+        vanilla javascript and styled and made it responsive in Sass.`,
+        tags: ["HTML", "Sass", "JavaScript"],
+        live: "https://hangman-game.intravaiaezio.co.uk/",
+        gitHub: "https://github.com/Ezio2994/hangman-game",
+    },
+    {
+        name: "Calculator",
+        urlImg: "./img/calculator.jpg",
+        description: `My first optional project was to build something using my JavaScript fundamental
+        so I opted for a common first project which is to build a calculator. <br> It is fully functional
+        and can be used both with keyboard and mouse.`,
+        tags: ["HTML", "Sass", "JavaScript"],
+        live: "http://calculator.intravaiaezio.co.uk/",
+        gitHub: "https://github.com/Ezio2994/Calculator",
+    },
+
+    // {
+    //     name: "",
+    //     urlImg: "",
+    //     description: ``,
+    //     tags: [""],
+    //     live: "",
+    //     gitHub: "",
+    // }
+]
+
+export default projects
--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-const navLinks = document.querySelectorAll('nav li')
+import projects from "./projectDatas.js"
+
 const navUl = document.querySelector('nav ul')
 const projectBox = document.querySelector('.projectsBox')
-const projects = document.querySelectorAll('.projectsBox div')
-const middleSection = document.querySelectorAll('.middleSection')
-const bottomSectionButtons = document.querySelectorAll('.bottomSection button')
+const projectsContainer = document.querySelector(".projectsBox div")
+const bottomSection = document.getElementsByClassName("bottomSection")
 const navbarButton = document.querySelector('#navBar')
-const navBar = document.querySelector('nav')
 
-let slide = 1
-let play = true
-let i;
-
+let index = 0
+let interval;
 
 window.onscroll = () => {
     if (window.innerWidth > 1100) {
@@ -44,82 +41,44 @@ navUl.onclick = () => {
     }
 }
 
+projects.forEach(project => bottomSection[0].innerHTML += `<button>${project.name}</button>`)
+projectBox.onmouseout = () => clearInterval(interval);
+projectBox.onmouseleave = () => startInt()
+const startInt = () => interval = setInterval(projectsGenerator, 4000)
 
 const resetButtons = () => {
-    bottomSectionButtons.forEach((button) => {
-        button.className = 'bottomButton'
-    })
-    projects.forEach((project) => {
-        project.style.display = 'none'
-    })
-}
-
-projectBox.onmouseout = () => play = false;
-projectBox.onmouseleave = () => play = true;
-
-const slideTimer = () => {
-    if (play === true && window.innerWidth > 900) {
-        if (slide === 1) {
-            project1Slide()
-        } else if (slide === 2) {
-            project2Slide()
-        } else if (slide === 3) {
-            project3Slide()
-        } else if (slide === 4) {
-            project4Slide()
-        } else if (slide === 5) {
-            project5Slide()
-            slide = 0
-        }
-
-        slide++
+    for (let i = 0; i < bottomSection[0].children.length; i++) {
+        bottomSection[0].children[i].removeAttribute("class")
     }
 }
 
-setInterval(slideTimer, 4000)
-
-const gradiant = "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5))"
-
-projects[0].style.display = 'block'
-bottomSectionButtons[0].addEventListener('click', project1Slide = () => {
-    projectBox.style.backgroundImage = `${gradiant}, url('./img/rpgGame.jpg')`
-    slide = 1;
+const projectsGenerator = () => {
     resetButtons()
-    projects[0].style.display = 'block'
-    bottomSectionButtons[0].classList = 'bottomButtonOn'
-})
+    bottomSection[0].children[index].setAttribute("class", "bottomButtonOn")
+    projectBox.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url("${projects[index].urlImg}")`
+    projectsContainer.innerHTML = `
+            <article class="middleSection slit-in-vertical">
+                <h2 class="fade">${projects[index].name}</h2>
+                <p class="fade">${projects[index].description}</p>
+                ${projects[index].tags.map(tag => `<span title="${tag}">${tag}</span>`)}<br>
+                <a href="${projects[index].live}" target='_blank'><i
+                        class="fas fa-code"></i></a>
+                <a href="${projects[index].gitHub}" target='_blank'><i
+                        class="fab fa-github-alt"></i></a>
+            </article>
+        `
+    index < projects.length - 1 ? index++ : index = 0;
+}
 
-bottomSectionButtons[1].addEventListener('click', project2Slide = () => {
-    projectBox.style.backgroundImage = `${gradiant},  url('./img/The-sicilian-shop.jpeg')`
-    slide = 2;
-    resetButtons()
-    projects[1].style.display = 'block'
-    bottomSectionButtons[1].classList = 'bottomButtonOn'
-})
+bottomSection[0].onclick = (e) => {
+    const projectNames = projects.map(project => project.name)
+    if (projectNames.indexOf(e.target.innerHTML) >= 0) {
+        resetButtons()
+        index = projectNames.indexOf(e.target.innerHTML)
+        projectsGenerator()
+    }
+}
 
-bottomSectionButtons[2].addEventListener('click', project3Slide = () => {
-    projectBox.style.backgroundImage = `${gradiant},  url('./img/music-quiz.jpg')`
-    slide = 3;
-    resetButtons()
-    projects[2].style.display = 'block'
-    bottomSectionButtons[2].classList = 'bottomButtonOn'
-})
-
-bottomSectionButtons[3].addEventListener('click', project4Slide = () => {
-    projectBox.style.backgroundImage = `${gradiant},  url('./img/hangman.jpg')`
-    slide = 4;
-    resetButtons()
-    projects[3].style.display = 'block'
-    bottomSectionButtons[3].classList = 'bottomButtonOn'
-})
-
-bottomSectionButtons[4].addEventListener('click', project5Slide = () => {
-    projectBox.style.backgroundImage = `${gradiant},  url('./img/calculator.jpg')`
-    slide = 5;
-    resetButtons()
-    projects[4].style.display = 'block'
-    bottomSectionButtons[4].classList = 'bottomButtonOn'
-})
-
-
+startInt()
+projectsGenerator()
 
